8f18c64f8e3531363372c15b45ebc395da1b4dc9
jleto6 <jletobar@gmail.com>
2025-02-23 18:45:24 -0500

    finished

--- pa01.py
+++ pa01.py
@@ -16,7 +16,7 @@
 +==================================================================='''
 
 import sys
-import math
+import textwrap
 
 # Get arguments
 key = sys.argv[1]
@@ -28,14 +28,13 @@ try:
         key_content = [line.strip().split() for line in file]  # Strip each line and make it a nested list
         matrix_size = int(key_content[0][0]) # Store the matrix size
         del key_content[0]  # Then delete the identifier from the key content
-        print("")
-        print(f"Key Content: \n{key_content}")  # Print  
+        #print(f"Key Content: \n{key_content}")  # Print  
 except:
     print("Error reading key text")
-print("")
 
 # Print the key
-print("The Key:")
+print()
+print("Key matrix:")
 try:
     for i in range(matrix_size):
         for j in range(0, matrix_size):
@@ -43,89 +42,83 @@ try:
         print("")    
 except:
     print("Error")
+print()
+  
 
 #--- Plaintext ---#
-
 # Open plaintext
 try: 
     with open(plaintext, 'r') as file:
         plaintext_content = file.read().replace("\n", "")
-        plaintext_content = "".join(c for c in plaintext_content if c.isalnum())
+        plaintext_content = "".join(c.lower() for c in plaintext_content if 'a' <= c.lower() <= 'z')
 except:
     print("Error reading key text")
 
-print("")
-print(f"Raw Plaintext Content: \n{plaintext_content}")
-print("")
+# Check if it needs padding
+remainder = (len(plaintext_content)) % matrix_size
+if remainder != 0:
+    padding_needed = matrix_size - remainder
+    plaintext_content += ('x') * padding_needed
+
+# Print out the plaintext 
+formatted_plaintext = textwrap.fill(plaintext_content, width=80)  # Wrap to 80 columns
+print(f"Plaintext:\n{formatted_plaintext}")
 
 # Dict storing character and its numerical value in alphabet
 char_buffer = {
     "character" : [],
     "numerical" : [],
 }
+
 # Convert the plaintext letters to numerical values
 i=0
 numerical_values = []
 for char in plaintext_content:
     char_buffer["character"].append(char)
-
-    # If current plaintext value is already a number (and note at which position its at for later)
-    if char.isdigit():
-        numerical_values.append(i)
-        char = int(char)
-        char_buffer["numerical"].append(char)
-    # Else, convert to number
-    else:
-        char = char.upper()
-        char = ord(char) - ord('A')
-        char_buffer["numerical"].append(char)
+    char = char.upper()
+    char = ord(char) - ord('A')
+    char_buffer["numerical"].append(char)
     i+=1
 i=0
 original_group_buffer = []
 group_buffer = []
 
 # Create a nested list for the plaintext groups
-for i in range (0, len(plaintext_content), 2,):
+for i in range (0, len(plaintext_content), matrix_size):
     try:
-        original_group_buffer.append([char_buffer["character"][i], char_buffer["character"][i+1]])
-        group_buffer.append([char_buffer["numerical"][i], char_buffer["numerical"][i+1]])
+        original_group_buffer.append(char_buffer["character"][i:i + matrix_size])
+        group_buffer.append(char_buffer["numerical"][i:i + matrix_size])
     except:
         print("out of range")
 
-group_length = (len(plaintext_content))/2
+group_length = (len(plaintext_content))/matrix_size
 group_length = int(group_length)
-
-print("")
-print(f"Grouped Numerical Plaintext Content: ")
-for i in range (0, group_length):
-    try: 
-        print(group_buffer[i])
-    except Exception as e:
-        print(f"Out of range:{e}")    
-print("")
+#print(f"Group buffer: {group_buffer}")
 
 # Multiply
-print("-----------------------")
-print("")
 
 #'''
 results = []
+#print(key_content)
 # Go through amount of plaintext
 for i in range((len(group_buffer))):
+    plaintext_vector = group_buffer[i]
+    #print(plaintext_vector)
     # Row of matrix
     for j in range(matrix_size):
-        current_plaintext = int(group_buffer[i][j])
         inner_list = []
         # Column of matrix
         for k in range(matrix_size):
-            current_key = int(key_content[j][k])
-            print(f"Multiplication: {current_plaintext} * {current_key}")
-            result = (current_plaintext * current_key)
-            inner_list.append(result)
+            try:
+                current_plaintext = int(plaintext_vector[k])
+                current_key = int(key_content[j][k])
+                #print(f"The curent plaintext{plaintext_vector[k]}", end="")
+                #print(f"Multiplication: {current_plaintext} * {current_key}")
+                result = (current_plaintext * current_key)
+                inner_list.append(result)
+            except Exception as e:
+                print(f"Out of range {e}")
         results.append(inner_list)
-print()
-#print(results)
-
 
 # Group results into lists
 main_grouped_list = []
@@ -139,11 +132,9 @@ while i < len(plaintext_content):
             print("out of RANGE")    
         i+=1
     main_grouped_list.append(grouped_list)
-print(main_grouped_list)
+#print(main_grouped_list)
 
 # Do arithmetic on groups
-#print(len(main_grouped_list))
-
 i = 0
 # Go list by list
 result_list = []
@@ -154,83 +145,30 @@ while i < len(main_grouped_list):
         try: 
             current = main_grouped_list[i][j]
             addition = sum(current)
-            print(addition)
+            #print(addition)
             modulo = (addition % 26)
-            print(modulo)
+            #print(modulo)
             current_list.append(modulo)
         except:
             print("out of RANGE")    
     i+=1
     result_list.append(current_list)
 
-print()
-#print(result_list)
-print()
-print("-----------------------")
-print()
-
 # Convert to ciphertext
+print()
 i=0
+ciphertext_content = ""
 for sublist in result_list:    # Outer elements
     for num in sublist:
-        if i in numerical_values: # If it was already a number
-            print(num, end="")
-        # Else, convert to letter
-        else:
-            character = num + ord('A')
-            character = chr(character)
-            character = character.lower()
-            print(character, end="")
+        character = num + ord('A')
+        character = chr(character)
+        character = character.lower()
+        ciphertext_content += character
         i+=1
-'''
-result0 = int(key_content[l][k]) * int(group_buffer[i][j])
 
-result1 = int(key_content[l][k+1]) * int(group_buffer[i][j])           
-
-print(f"{i} | {key_content[l][k]}*{group_buffer[i][j]}|{original_group_buffer[i][j]} = {result0}")
-
-print(f"{i} | {key_content[l][k+1]}*{group_buffer[i][j]}|{original_group_buffer[i][j]} = {result1}")
-
-final_result0 = ((result0 + result1)%26)
-final_result0 = final_result0 + ord('A')
-
-print(f"The final result is: {chr(final_result0)}")
-print("")
-'''
-#''' 
-
-"""
-result0 = int(key_content[1][0]) * int(char_buffer["numerical"][0])
-result1 = int(key_content[1][1]) * int(char_buffer["numerical"][1])
-final_result0 = chr(((result0 + result1)%26) + ord('A'))
-print(final_result0, end="")
-
-result2 = int(key_content[2][0]) * int(char_buffer["numerical"][0])
-result3 = int(key_content[2][1]) * int(char_buffer["numerical"][1])
-final_result1 = chr(((result2 + result3)%26) + ord('A'))
-print(final_result1, end = "")
-"""
-
-'''
-print(result0)
-print(result1)
-print(result2)
-print(result3)
-'''
-
-
-"""
-print(f"key content: {key_content[i][j]}")
-print(f"char buffer: {char_buffer["numerical"][1]}")
-result = int(key_content[i][j]) * int(char_buffer["numerical"][1])
-print(f"RESULT: {result}")
-"""
-
-print("")
-
-print("")
-
-
+# Print the ciphertext
+formatted_ciphertext = textwrap.fill(ciphertext_content, width=80)  # Wrap to 80 columns
+print(f"Ciphertext:\n{formatted_ciphertext}")
 
 '''=============================================================================
 | I Joseph Letobar 5658506 affirm that this program is
